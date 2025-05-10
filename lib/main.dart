432a7088abb62cd666aea6ebe0b54adc87dde2d8
Gsdagustavo <gugadanielalvez@gmail.com>
2025-05-10 18:52:01 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat',
+      debugShowCheckedModeBanner: false,
+
+      routes: {
+        '/':(_) => const HomePage()
+      }
+    );data
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Hello World!')
+      )
+    );
+  }
+}
