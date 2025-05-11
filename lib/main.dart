c6d96a0ed17d1725fefde0e9b104e89cb5cfa383
Gsdagustavo <gugadanielalvez@gmail.com>
2025-05-10 21:14:51 -0300

    created and populated the db.json file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,34 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
-void main() {
+import 'package:http/http.dart' as http;
+
+import 'model/user.dart';
+
+const String getUsersUrl = 'http://localhost:3000/users';
+
+
+
+Future<List<User>> getUsers() async {
+  final List<User> users = [];
+
+  final response = await http.get(Uri.parse(getUsersUrl));
+
+  if (response.statusCode == 200) {
+    final resp = jsonDecode(response.body);
+
+    for (final userJson in resp) {
+      final user = User.fromJson(userJson);
+      users.add(user);
+    }
+  }
+
+  return users;
+}
+
+void main() async {
+  await getUsers();
   runApp(const MyApp());
 }
 
@@ -23,11 +51,36 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  @override 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hello World!')
+      body: Column(
+        children: [
+
+          Expanded(
+            child: FutureBuilder(
+              future: getUsers(), 
+              builder: (context, snapshot) {
+                if (snapshot.hasError || !snapshot.hasData) {
+                  return Container(color: Colors.red);
+                }
+
+                final users = snapshot.data!;
+
+                return ListView.builder(
+                  itemCount: users.length,
+                  itemBuilder: (context, index) {
+                    final user = users[index];
+
+                    return ListTile(
+                      leading: Image.network(user.image),
+                    );
+                  }
+                );
+              }
+            )
+          )
+        ]
       )
     );
   }
