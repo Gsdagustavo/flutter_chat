2d1c00a16a17af428d77211ca631244bb5d98a60
Gsdagustavo <gugadanielalvez@gmail.com>
2025-05-11 08:34:08 -0300

    created a separated file to the app widget, and renamed it to ChatApp

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat/views/home_page.dart';
+
+class ChatApp extends StatelessWidget {
+  const ChatApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat',
+      debugShowCheckedModeBanner: false,
+
+      routes: {
+        '/':(_) => HomePage()
+      }
+    );
+  }
+}
